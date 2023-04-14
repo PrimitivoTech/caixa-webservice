76aca17513803bfac88607cbd56ab06a61d0dbc8
Matheus Santos <matheussantos@devsquad.com>
2023-04-14 19:05:55 -0300

    Added getters/setters for `nome` and `documento` Added some tests for `Agent` class

--- src/Agente.php
+++ src/Agente.php
@@ -2,16 +2,53 @@
 
 namespace Primitivo\Caixa;
 
+use InvalidArgumentException;
+
 class Agente
 {
-    public function __construct(
-        protected string $nome,
-        protected string $documento,
-        protected ?string $logradouro,
-        protected ?string $bairro,
-        protected ?string $cidade,
-        protected ?string $estado,
-        protected ?string $cep,
-    ) {
+    protected string $nome;
+
+    protected string $documento;
+
+    protected ?string $logradouro = null;
+
+    protected ?string $bairro = null;
+
+    protected ?string $cidade = null;
+
+    protected ?string $estado = null;
+
+    protected ?string $cep = null;
+
+    public function __construct(string $nome, string $documento)
+    {
+        $this->setNome($nome);
+        $this->setDocumento($documento);
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        if (strlen($nome) > 40) {
+            throw new InvalidArgumentException('O nome deve conter, no máximo, 40 caracteres.');
+        }
+
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getDocumento(): string
+    {
+        return $this->documento;
+    }
+
+    public function setDocumento(string $documento): void
+    {
+        $this->documento = $documento;
     }
 }
---
+++ tests/Unit/AgentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Primitivo\Caixa\Agente;
+
+use function PHPUnit\Framework\assertEquals;
+
+it('should ensure that name and document are required for a new instance', function () {
+    $agente = new Agente('João da Silva', '27431897111');
+
+    assertEquals('João da Silva', $agente->getNome());
+    assertEquals('27431897111', $agente->getDocumento());
+});
+
+it('should ensure that the name does not have more than forty chars for name', function () {
+    $this->expectException(InvalidArgumentException::class);
+    $this->expectExceptionMessage('O nome deve conter, no máximo, 40 caracteres.');
+
+    new Agente(str_repeat('a', 41), '27431897111');
+});
