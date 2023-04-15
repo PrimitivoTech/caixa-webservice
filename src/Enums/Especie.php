173c0a12707b36fe5bf8415c24f8631a07c834ad
Matheus Santos <matheussantos@devsquad.com>
2023-04-15 13:27:48 -0300

    Added Especie enum

---
+++ src/Enums/Especie.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Primitivo\Caixa\Enums;
+
+enum Especie: string
+{
+    case CH  = '01';
+    case DM  = '02';
+    case DMI = '03';
+    case DS  = '04';
+    case DSI = '05';
+    case DR  = '06';
+    case LC  = '07';
+    case NCC = '08';
+    case NCE = '09';
+    case NCI = '10';
+    case NCR = '11';
+    case NP  = '12';
+    case NPR = '13';
+    case TM  = '14';
+    case TS  = '15';
+    case NS  = '16';
+    case RC  = '17';
+    case FAT = '18';
+    case ND  = '19';
+    case AP  = '20';
+    case ME  = '21';
+    case PC  = '22';
+    case NF  = '23';
+    case DD  = '24';
+    case CPR = '25';
+    case CC  = '31';
+    case BP  = '32';
+    case OU  = '99';
+}
