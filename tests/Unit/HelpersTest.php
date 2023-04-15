d6acb24b8d4e7336a9fdf8a6a6e44245b6906334
Matheus Santos <matheussantos@devsquad.com>
2023-04-15 11:30:26 -0300

    Added tests that validates cpf

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -19,7 +19,7 @@ it('should remove accents from a string', function () {
     assertEquals('Jose Moncao da Silva Nho', Helpers::unaccents($str));
 });
 
-it('should ensure that CNPJ are valid', function () {
+it('should ensure that CNPJ is valid', function () {
     assertTrue(Helpers::isCnpj('63.024.642/0001-40'));
     assertFalse(Helpers::isCnpj('00.000.000/0000-00'));
     assertFalse(Helpers::isCnpj('11.111.111/1111-11'));
@@ -32,4 +32,19 @@ it('should ensure that CNPJ are valid', function () {
     assertFalse(Helpers::isCnpj('88.888.888/8888-88'));
     assertFalse(Helpers::isCnpj('99.999.999/9999-99'));
     assertFalse(Helpers::isCnpj('63.024.642'));
+});
+
+it('should ensure that CPF is valid', function () {
+    assertTrue(Helpers::isCpf('570.616.604-81'));
+    assertFalse(Helpers::isCnpj('000.000.000-00'));
+    assertFalse(Helpers::isCnpj('111.111.111-11'));
+    assertFalse(Helpers::isCnpj('222.222.222-22'));
+    assertFalse(Helpers::isCnpj('333.333.333-33'));
+    assertFalse(Helpers::isCnpj('444.444.444-44'));
+    assertFalse(Helpers::isCnpj('555.555.555-55'));
+    assertFalse(Helpers::isCnpj('666.666.666-66'));
+    assertFalse(Helpers::isCnpj('777.777.777-77'));
+    assertFalse(Helpers::isCnpj('888.888.888-88'));
+    assertFalse(Helpers::isCnpj('999.999.999-99'));
+    assertFalse(Helpers::isCnpj('570.616.604'));
 });
