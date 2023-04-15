dfbe33b99799359ed79da157cd9e780028a64b0b
Matheus Santos <matheussantos@devsquad.com>
2023-04-15 13:08:35 -0300

    Improved cpf and cnpj checking

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -19,6 +19,10 @@ it('should remove accents from a string', function () {
     assertEquals('Jose Moncao da Silva Nho', Helpers::unaccents($str));
 });
 
+it('should return null if the string is empty or null when removing accents', function (?string $text) {
+    assertNull(Helpers::unaccents($text));
+})->with(['', null]);
+
 it('should ensure that CNPJ is valid', fn (string $cnpj) => assertTrue(Helpers::isCnpj($cnpj)))
     ->with([
         '63.024.642/0001-40',
@@ -27,6 +31,7 @@ it('should ensure that CNPJ is valid', fn (string $cnpj) => assertTrue(Helpers::
         '01.652.592/0001-99',
     ]);
 
+/** @covers Helpers::isCnpj() */
 it('should ensure that CNPJ is invalid', fn (string $cnpj) => assertFalse(Helpers::isCnpj($cnpj)))
     ->with([
         '00.000.000/0000-00',
@@ -40,6 +45,8 @@ it('should ensure that CNPJ is invalid', fn (string $cnpj) => assertFalse(Helper
         '88.888.888/8888-88',
         '99.999.999/9999-99',
         '63.024.642',
+        'abcdef',
+        '123456789012345'
     ]);
 
 it('should ensure that CPF is valid', fn (string $cpf) => assertTrue(Helpers::isCpf($cpf)))
@@ -47,20 +54,23 @@ it('should ensure that CPF is valid', fn (string $cpf) => assertTrue(Helpers::is
         '570.616.604-81',
         '766.214.797-51',
         '211.897.202-48',
-        '577.976.329-10'
+        '577.976.329-10',
     ]);
 
-it('should ensure that CPF is invalid', fn (string $cpf) => assertFalse(Helpers::isCnpj($cpf))
-)->with([
-    '000.000.000-00',
-    '111.111.111-11',
-    '222.222.222-22',
-    '333.333.333-33',
-    '444.444.444-44',
-    '555.555.555-55',
-    '666.666.666-66',
-    '777.777.777-77',
-    '888.888.888-88',
-    '999.999.999-99',
-    '570.616.604',
-]);
+/** @covers Helpers::isCpf() */
+it('should ensure that CPF is invalid', fn (string $cpf) => assertFalse(Helpers::isCnpj($cpf)))
+    ->with([
+        '000.000.000-00',
+        '111.111.111-11',
+        '222.222.222-22',
+        '333.333.333-33',
+        '444.444.444-44',
+        '555.555.555-55',
+        '666.666.666-66',
+        '777.777.777-77',
+        '888.888.888-88',
+        '999.999.999-99',
+        '570.616.604',
+        'abcdef',
+        '123456789012'
+    ]);
