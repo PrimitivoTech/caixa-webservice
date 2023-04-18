f13c42c6fbb9b85e48d4673925f7c08d6d8b91fe
Matheus Santos <matheussantos@devsquad.com>
2023-04-17 22:05:59 -0300

    Added getters and setters for convenio and beneficiario

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,11 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Primitivo\Caixa;
 
 use Carbon\Carbon;
+use InvalidArgumentException;
+use Primitivo\Caixa\Enums\Operacao;
+use Primitivo\Caixa\Enums\VersaoBoleto;
 
 class Boleto
 {
+    protected const USUARIO_SERVICO = 'SGCBS02P';
+
+    protected const SISTEMA_ORIGEM = 'SIGCB';
+
     protected const MOEDA = '09';
 
     protected string $convenio;
@@ -37,4 +46,41 @@ class Boleto
     protected array $reciboPagador = [];
 
     protected ?Agente $pagador = null;
+
+    protected VersaoBoleto $versaoBoleto = VersaoBoleto::CONVENCIONAL;
+
+    protected Operacao $operacao = Operacao::INCLUSAO;
+
+    public function getConvenio(): string
+    {
+        return $this->convenio;
+    }
+
+    public function setConvenio(string $convenio): static
+    {
+        $this->convenio = str_pad($convenio, 7, '0', STR_PAD_LEFT);
+
+        return $this;
+    }
+
+    public function getCnpjBeneficiario(): string
+    {
+        return $this->cnpjBeneficiario;
+    }
+
+    public function setCnpjBeneficiario(string $cnpjBeneficiario): static
+    {
+        $cnpjBeneficiario = Helpers::unmask($cnpjBeneficiario);
+
+        $isCpf  = Helpers::isCpf($cnpjBeneficiario);
+        $isCnpj = Helpers::isCnpj($cnpjBeneficiario);
+
+        if (!$isCpf && !$isCnpj) {
+            throw new InvalidArgumentException('O documento do beneficiário é inválido.');
+        }
+
+        $this->cnpjBeneficiario = $cnpjBeneficiario;
+
+        return $this;
+    }
 }
---
+++ tests/Unit/BoletoTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Primitivo\Caixa\Boleto;
+
+use function PHPUnit\Framework\assertEquals;
+
+it('should set the convenio', function () {
+    $boleto = new Boleto();
+    $boleto->setConvenio(1234567);
+
+    assertEquals('1234567', $boleto->getConvenio());
+});
+
+it('should fill convenio with zero if its length is lower than seven', function () {
+    $boleto = new Boleto();
+    $boleto->setConvenio(12345);
+
+    assertEquals('0012345', $boleto->getConvenio());
+});
+
+it('should set the CNPJ of the recipient', function () {
+    $boleto = new Boleto();
+    $boleto->setCnpjBeneficiario('76.134.272/0001-46');
+
+    assertEquals('76134272000146', $boleto->getCnpjBeneficiario());
+});
+
+it('should set the CPF of the recipient', function () {
+    $boleto = new Boleto();
+    $boleto->setCnpjBeneficiario('826.528.431-41');
+
+    assertEquals('82652843141', $boleto->getCnpjBeneficiario());
+});
+
+it('should throw an exception if CNPJ is invalid', function (string $cnpj) {
+    $this->expectException(InvalidArgumentException::class);
+    $this->expectExceptionMessage('O documento do beneficiário é inválido.');
+
+    $boleto = new Boleto();
+    $boleto->setCnpjBeneficiario($cnpj);
+})->with(['11.111.111/1111-11', '11.111.111', '111.111.111-11', '111.111.111']);
